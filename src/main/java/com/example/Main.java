b26766019fa14c77176184a1cb07e92870bba45f
Daniele Pasquini <daniele.pasquini@sistinf.it>
2018-08-09 11:28:50 +0200

    Prova rest

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -66,6 +66,7 @@ public class Main {
   }
 
   @GetMapping("/hello-world")
+  @ResponseBody
   public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
 	return new Greeting(counter.incrementAndGet(), String.format(template, name));
   }
