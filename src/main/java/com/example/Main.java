cd5b5541d689748470f13f189198525e28435d3d
Daniele Pasquini <daniele.pasquini@sistinf.it>
2018-08-09 11:37:51 +0200

    Prova aggiunto rest pdf

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 
 import javax.sql.DataSource;
